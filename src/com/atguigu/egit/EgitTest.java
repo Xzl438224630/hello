9deb6690f716c7d11b2912a39d131294702b1e7a
Xzl438224630 <55442599+Xzl438224630@users.noreply.github.com>
2019-09-19 11:16:41 +0800

    Update EgitTest.java

--- src/com/atguigu/egit/EgitTest.java
+++ src/com/atguigu/egit/EgitTest.java
@@ -4,5 +4,6 @@ public class EgitTest {
 	public static void main(String[] args) {
 		System.out.println("*****************************");
 		System.out.println("更新啦*.*");
+		//开发一个新功能，可以无限续杯82拉菲
 	}
 }
